ac6b1dde119a6d193987fff697b2f5c640bc5503
Igor Stepanenko <igor.stepanenko.93@gmail.com>
2018-07-23 13:21:08 +0300

    More wide worlds configuration -- now YAML
    Make worlds search dynamically using seed generated token.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::fs::File;
 use std::env::args;
 
 extern crate serde;
-extern crate serde_json;
+//extern crate serde_json;
+extern crate serde_yaml;
 
 #[macro_use]
 extern crate serde_derive;
@@ -14,14 +15,14 @@ fn main() {
 
     let arguments: Vec<String> = args().collect();
     println!("Arguments:  {} {}", arguments[0], arguments[1]);
-    //TODO: load configuration
-    let worlds : Vec<World> = serde_json::from_reader(File::open(arguments[1].clone()).unwrap()).expect("Failed to load config");
-    println!("Worlds: {:?}", worlds);
-    return;
+    //let worlds : Vec<World> = serde_json::from_reader(File::open(arguments[1].clone()).unwrap()).expect("Failed to load config");
+    let worlds : Vec<World> = serde_yaml::from_reader(File::open(arguments[1].clone()).unwrap()).expect("Failed to load config");
+    //println!("Worlds: {:?}", worlds);
+    //return;
 
     println!("Добро пожаловать в мрачное будущее!");
 
-    let world = loop {
+    let world: &World = loop {
         let mut input = String::new();
         println!("Выбери свой родной мир: ");
         io::stdin().read_line(&mut input).expect("Failed to read line!");
@@ -33,7 +34,7 @@ fn main() {
                 continue;
             },
             Ok(seed) => {
-                match generate_world(seed) {
+                match filter_world(seed, &worlds) {
                     Err(err) => {
                         println!("{}", err);
                         continue;
@@ -50,7 +51,7 @@ fn main() {
     println!("Вы можете сгенерировать характеристики с помощью генератора случайных чисел \
         или автоматически.");
 
-    println!("Выберите свою профессию. Для вашего персонажа доступны:");
+    println!("Выберите свою профессию. Для вашего персонажа доступны: {:?}", world.classes);
 
     println!("Навыки и стартовая экипировка:");
 
@@ -59,15 +60,18 @@ fn main() {
 
 #[derive (Serialize, Deserialize, Debug)]
 struct World {
+    token: String,
     name: String,
-    //description: String,
+    classes: Vec<String>,
+    skills: Vec<String>,
+    features: Vec<String>,
 }
 
 struct Character {
     description: String,
 }
 
-fn generate_world(seed: u8) -> Result<World, String> {
+/*fn generate_world(seed: u8, worlds: &Vec<World>) -> Result<World, String> {
     if seed > 100 {
         Err(String::from("Значение параметра слишком велико!"))
     } else if seed > 95 {
@@ -88,5 +92,43 @@ fn generate_world(seed: u8) -> Result<World, String> {
         Ok(World {name: String::from("Дикий мир")})
     } else {
         Err(String::from("Число не может быть нулём!"))
+    }
+}*/
+
+fn generate_token(seed: u8) -> Result<String, String> {
+    if seed > 100 {
+        Err(String::from("Значение параметра слишком велико!"))
+    } else if seed > 95 {
+        Ok("E".to_owned()) // Empty mind
+    } else if seed > 85 {
+        Ok("N".to_owned()) // Noble blood
+    } else if seed > 75 {
+        Ok("S".to_owned()) // Schola Progenium
+    } else if seed > 65 {
+        Ok("F".to_owned()) // Forge world
+    } else if seed > 55 {
+        Ok("V".to_owned()) // Born in Void
+    } else if seed > 35 {
+        Ok("I".to_owned()) // Imperium world
+    } else if seed > 15 {
+        Ok("H".to_owned()) // Hive
+    } else if seed > 0 {
+        Ok("W".to_owned()) // Wild world
+    } else {
+        Err(String::from("Число не может быть нулём!"))
+    }
+}
+
+fn filter_world(seed: u8, config: &Vec<World>) -> Result<&World, String> {
+    let token = match generate_token(seed) {
+        Ok(t) => t,
+        Err(e) => return Err(e)
+    };
+    let world = config.iter().find(|w| {
+        w.token == token
+    });
+    match world {
+        Some(w) => Ok(w),
+        None => Err(format!("No token {} in current configuration!", token))
     }
 }
